7e535e7cd1d778482fb7c7c89ddaf4a8b4550ed2
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-06-22 09:18:20 +0200

    Searchbar has been changed to function component

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { BsSearch } from 'react-icons/bs';
@@ -10,39 +9,38 @@ import {
   SearchInput,
 } from './Searchbar.styled';
 
-class Searchbar extends Component {
-  handleSubmit = e => {
+const Searchbar = ({ onClickSearch }) => {
+  const handleSubmit = e => {
     e.preventDefault();
     const searchQuery = e.currentTarget.elements.searchQuery.value
       .trim()
       .toLowerCase();
-    this.props.onClickSearch(searchQuery);
+    onClickSearch(searchQuery);
   };
 
-  render() {
-    return (
-      <Header className="searchbar">
-        <SearchForm className="form" onSubmit={this.handleSubmit}>
-          <SearchButton type="submit" className="button">
-            <BsSearch width="16" height="16" />
-          </SearchButton>
+  return (
+    <Header className="searchbar">
+      <SearchForm className="form" onSubmit={handleSubmit}>
+        <SearchButton type="submit" className="button">
+          <BsSearch width="16" height="16" />
+        </SearchButton>
 
-          <SearchInput
-            className="input"
-            type="text"
-            name="searchQuery"
-            autocomplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </SearchForm>
-      </Header>
-    );
-  }
-}
+        <SearchInput
+          className="input"
+          type="text"
+          name="searchQuery"
+          autocomplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+    </Header>
+  );
+  // }
+};
 
 export default Searchbar;
 
 Searchbar.propTypes = {
-  onSubmit: PropTypes.func,
+  onSubmit: PropTypes.func.isRequired,
 };
